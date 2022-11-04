ea30aaae1e2fb42b13453a118d2d6ad2faeac397
Andra <andra.popovici@gradintel.com>
2022-11-04 13:55:12 +0000

    cypress new test

---
+++ cypress/e2e/2-advanced-examples/testproj.cy.js
@@ -0,0 +1,13 @@
+context('Google serach', () => {
+    beforeEach(() => {
+      cy.visit('http://www.google.com')
+    })
+
+
+describe('Visit site', () => {
+    it('passes', () => {
+      cy.get()
+    })
+  })
+
+})
